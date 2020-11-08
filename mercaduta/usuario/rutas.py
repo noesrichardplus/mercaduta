e3b4ee03ff2b5005b3e1024e314afa8ebc629cc8
Richard Carrion <richard.sna@hotmail.com>
2020-11-08 08:42:57 -0500

    Mis ofertas y mi perfi parece que esta arreglado

--- mercaduta/usuario/rutas.py
+++ mercaduta/usuario/rutas.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,session
 
 import mercaduta.db_query as db_query 
 
@@ -6,7 +6,7 @@ usuario = Blueprint('usuario',__name__,template_folder='templates')
 
 @usuario.route('/miperfil')
 def mi_perfil():
-    email = 'rcarrion3406@uta.edu.ec' 
+    email = session['email']
     usuario = db_query.get_usuario(email)
     return render_template("miperfil.html",usuario = usuario)
 
@@ -14,6 +14,6 @@ def mi_perfil():
 
 @usuario.route('/misofertas')
 def mis_ofertas(): 
-    email = 'rcarrion3406@uta.edu.ec'
+    email = session['email']
     ofertas = db_query.get_ofertas(email)
     return render_template("misofertas.html",ofertas = ofertas)
