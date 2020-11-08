56d9f58c71a2d89ea27f0420fad295095026fc3b
Richard Carrion <richard.sna@hotmail.com>
2020-11-08 00:20:45 -0500

    Modificando

--- mercaduta/config.py
+++ mercaduta/config.py
@@ -2,14 +2,9 @@ import os
 import sys 
 import urllib.parse as urlparse
 
-urlparse.uses_netloc.append('mysql')
 
-url = urlparse.urlparse(os.environ['CLEARDB_DATABASE_URL'])
-
-BD = url.path[1:]
-USER = url.username
-PASSWD = url.password
-HOST = url.hostname
-PORT = url.port
-
+BD = "mercaduta"
+USER = "root"
+PASSWD = "123123qweqwe.r"
+HOST = "localhost"
 
