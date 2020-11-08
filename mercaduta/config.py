b18cdff5204c60c052015e2f664f4e5901bc8fde
Richard Carrion <richard.sna@hotmail.com>
2020-11-08 00:35:40 -0500

    change data for db

--- mercaduta/config.py
+++ mercaduta/config.py
@@ -2,9 +2,12 @@ import os
 import sys 
 import urllib.parse as urlparse
 
+urlparse.uses_netloc.append('mysql')
 
-BD = "mercaduta"
-USER = "root"
-PASSWD = "123123qweqwe.r"
-HOST = "localhost"
+url = urlparse.urlparse(os.environ['CLEARDB_DATABASE_URL'])
 
+BD = url.path[1:]
+USER = url.username
+PASSWD = url.password
+HOST = url.hostname
+PORT = url.port
