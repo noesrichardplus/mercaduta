a32e875e252672e52e708fa3304b3cc2ad814fcf
noesrichard <73971226+noesrichard@users.noreply.github.com>
2020-11-11 08:39:45 -0500

    Cambios

--- mercaduta/config.py
+++ mercaduta/config.py
@@ -5,11 +5,11 @@ from flask import Config
 urlparse.uses_netloc.append('mysql')
 url = urlparse.urlparse(os.environ['CLEARDB_DATABASE_URL'])
 class HerokuDB_Config(Config):     
-    MYSQL_DB = url.path[1:]
-    MYSQL_USER = url.username
-    MYSQL_PASSWORD = url.password
-    MYSQL_HOST = url.hostname
-    MYSQL_PORT = url.port
+    MYSQL_DATABASE_DB = url.path[1:]
+    MYSQL_DATABASE_USER = url.username
+    MYSQL_DATABASE_PASSWORD	 = url.password
+    MYSQL_DATABASE_HOST = url.hostname
+    MYSQL_DATABASE_PORT = url.port
 
 '''
 class LocalDB_Config(Config): 
