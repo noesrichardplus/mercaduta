3fa5614ce18994c9e2f1d0de7fef416db29df26d
noesrichard <73971226+noesrichard@users.noreply.github.com>
2020-11-11 08:57:56 -0500

    Deshaciendo cambios en config

--- mercaduta/config.py
+++ mercaduta/config.py
@@ -3,32 +3,18 @@ import sys
 import urllib.parse as urlparse
 from flask import Config
 
-class HerokuDB_Config(Config):  
-    def __init__(self): 
-        urlparse.uses_netloc.append('mysql')
-        self.url = urlparse.urlparse(os.environ['CLEARDB_DATABASE_URL'])  
-    @property
-    def MYSQL_DATABASE_DB(self): 
-        return self.url.path[1:]
-    @property
-    def MYSQL_DATABASE_USER(self): 
-        return self.url.username
-    @property
-    def MYSQL_DATABASE_PASSWORD(self): 
-        return self.url.password
-    @property
-    def MYSQL_DATABASE_HOST(self): 
-        return self.url.hostname
-    @property
-    def MYSQL_DATABASE_PORT(self): 
-        return self.url.port
+class HerokuDB_Config(Config): 
+    urlparse.uses_netloc.append('mysql')
+    url = urlparse.urlparse(os.environ['CLEARDB_DATABASE_URL'])
+    MYSQL_DB = url.path[1:]
+    MYSQL_USER = url.username
+    MYSQL_PASSWORD = url.password
+    MYSQL_HOST = url.hostname
+    MYSQL_PORT = url.port
 
-     
-    
-'''
+
 class LocalDB_Config(Config): 
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
     MYSQL_PASSWORD = ""
     MYSQL_DB = "mercaduta"
-'''
