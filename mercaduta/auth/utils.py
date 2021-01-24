73a0db11e72fde2774e0f3b59ba457c31efc0416
noesrichard <73971226+noesrichard@users.noreply.github.com>
2021-01-24 14:47:25 -0500

    Agregando clases
    * Refactor solicitudes
    
    * Refactor a categorias
    
    * Corrigiendo errores
    
    * Corrigiendo errores
    
    * Corrigiendo errores
    
    * Corrigiendo errores
    
    * Corrigiendo errores
    
    * Refactoring

--- mercaduta/auth/utils.py
+++ mercaduta/auth/utils.py
@@ -5,30 +5,6 @@ from mercaduta import mail
 from functools import wraps
 import threading 
 
-def verificar_email(email): 
-    regex = "[^@]."
-    no_empieza = re.match(regex,email)
-    si_hay = re.search("@",email)
-    if no_empieza and si_hay:
-        email_divi = re.split("@",email)
-        if email_divi[1] == "uta.edu.ec": 
-            return True
-        else: 
-            return False
-    else: 
-        return False
-
-def verificar_registro(nombre,apellido,passwd,repe_passwd): 
-    regex_nombres = "[^\ ][a-zA-Z]"
-    nombre_valido = re.search(regex_nombres,nombre)
-    apellido_valido = re.search(regex_nombres,apellido)
-    if nombre_valido and apellido_valido and verify_passwd(passwd): 
-        if passwd == repe_passwd: 
-            return True
-        else: 
-            return False
-    else: 
-        return False
 
 def verify_passwd(passwd): 
     regex = "^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?.&])[A-Za-z\d@$!#%*?.&]{6,20}$"
@@ -45,7 +21,6 @@ def enviar_email(to,code):
     msg.body = f"El codigo para verificar tu cuenta es: {code}" 
     hilo = threading.Thread(target=enviar_email_async, args=[app,msg])
     hilo.start()
-
 
 
 def login_required(f):
